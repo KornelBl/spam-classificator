97d117dd86be1c08828d2c1d85843488968f94ab
Kornel <kornelblakala@wp.pl>
2022-05-24 00:20:27 +0200

    testy i obrazki

--- train.py
+++ train.py
@@ -3,10 +3,14 @@ from src.dataloader import DataLoader, DataConfig
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from scipy.stats import ttest_rel
 # from imblearn.pipeline import make_pipeline
+from tabulate import tabulate
 from sklearn.pipeline import make_pipeline, Pipeline
 import numpy as np
 import time
+import pandas as pd
+import matplotlib.pyplot as plt
 rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5,
                                random_state=43474692)
 
@@ -19,19 +23,19 @@ data_config = DataConfig(
 dl = DataLoader(data_config)
 X, y = dl.get_data()
 
+parametry = [16, 32, 64, 128, 256, 512, 1024, 2048]
 
-
-n_estimators = [int(x) for x in np.linspace(start=20, stop=50, num=2)]
-max_depth = [int(x) for x in np.linspace(20, 90, num=3)]
+n_estimators = [20]
+max_depth = [20]
 vectorizers = {
     "countvectorizer": CountVectorizer,
     "tfidfvectorizer": TfidfVectorizer
 }
 params = {'model__n_estimators': n_estimators,
           'model__max_depth': max_depth,
-          'vectorizer__max_features':[256,512,1024]}
+          'vectorizer__max_features': parametry}
 
-
+results = {}
 for key, vectorizer in vectorizers.items():
 
     pipe = Pipeline([("vectorizer",vectorizer()),
@@ -48,4 +52,44 @@ for key, vectorizer in vectorizers.items():
     models = grid.cv_results_["params"]
     scores = grid.cv_results_["mean_test_score"]
 
+    _results = []
+    for i in range(10):
+        _results.append(grid.cv_results_[f"split{i}_test_score"])
 
+    _results = np.array(_results).transpose()
+    results[key] = _results
+
+for key, values in results.items():
+    plt.boxplot(values.T)
+    plt.xticks([1, 2, 3, 4, 5, 6, 7, 8],parametry)
+    plt.savefig(f"imgs/{key}_boxplot.png")
+    plt.close()
+
+for key, values in results.items():
+    plt.plot(parametry, values.mean(axis=1))
+plt.legend(results.keys())
+plt.xscale('log')
+plt.xticks(parametry,parametry)
+plt.ylim(0.7, 0.9)
+plt.savefig("imgs/log_mean_accuracies.png")
+plt.close()
+
+
+def is_better(ttest_results, alpha=0.05):
+    if ttest_results.statistic > 0 and ttest_results.pvalue < alpha:
+        return True
+    else:
+        return False
+
+for key, values in results.items():
+    df = pd.DataFrame()
+    df["num"] = parametry
+    df["results"] = list(results["tfidfvectorizer"])
+    df["mean_acc"] = [np.mean(r) for r in df["results"]]
+    df["std_acc"] = [np.std(r) for r in df["results"]]
+    df["better"] = \
+        [[i for i, r in enumerate(df["results"]) if is_better(ttest_rel(result, r), alpha=0.05)] for result in df["results"]]
+
+    df = df.drop("results", axis=1)
+    print(f"Wyniki testów statystycznych dla {key}:")
+    print(tabulate(df, headers='keys'))
